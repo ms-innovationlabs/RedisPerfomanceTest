436223410aa33891996c6c51622482673863b92b
Andrey Surzh <andreysurzh@Mac.localdomain>
2024-09-25 16:18:32 +0300

    add env, update test

--- test.mjs
+++ test.mjs
@@ -2,6 +2,9 @@ import { createClient } from "redis";
 import crypto from "crypto";
 import { ObjectId } from "mongodb";
 import { performance } from "perf_hooks";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // Конфигурация AES-256-CBC
 const algorithm = "aes-256-cbc";
@@ -17,31 +20,42 @@ function encrypt(text) {
 }
 
 const client = createClient({
-  url: "redis://localhost:6379",
+  url: process.env.REDIS_URL,
 });
 
-let savedUsers = [];  // Сохраняем всех пользователей при генерации данных
+let savedUsers = []; // Сохраняем всех пользователей при генерации данных
 
 // Функция для создания данных и записи их в Redis
 async function createData() {
-
-  const createOrganizationCount = 1000;
+  const createOrganizationCount =  process.env.ORGANIZATIOS_COUNT || 10;
   try {
     await client.connect();
     for (let i = 0; i < createOrganizationCount; i++) {
       const orgId = new ObjectId().toString();
       const userCount = Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
-      const users = Array.from({ length: userCount }, (_, j) => encrypt(`user_${i}_${j}`));
+      const users = Array.from({ length: userCount }, (_, j) =>
+        encrypt(`user_${i}_${j}`)
+      );
 
       // Запись в test_kv (userID → orgID)
+      if (process.env.TEST_KV === "true") {
+        for (const userId of users) {
+          await client.set(`test_kv:${userId}`, orgId);
+        }
+      }
+
+      // Всегда сохраняем пользователей для тестирования
       for (const userId of users) {
-        await client.set(`test_kv:${userId}`, orgId); 
-        savedUsers.push({ userId, orgId });  // Сохраняем пользователей для теста
+        savedUsers.push({ userId, orgId });
       }
 
       // Запись в test_json (orgID → массив userID)
-      await client.json.set(`test_json:${orgId}`, "$", users);
-      console.log(`Organization ${orgId} with ${userCount} users added to Redis.`);
+      if (process.env.TEST_JSON === "true") {
+        await client.json.set(`test_json:${orgId}`, "$", users);
+      }
+      console.log(
+        `Organization ${orgId} with ${userCount} users added to Redis.`
+      );
     }
     console.log("-- Data creation complete. -- ");
     console.log("Created users count:", savedUsers.length);
@@ -58,7 +72,9 @@ async function measureExecutionTime(queryFunction, ...args) {
   const start = performance.now();
   const result = await queryFunction(...args);
   const end = performance.now();
-  return { result, time: end - start };
+  const time = end - start;
+  console.log("Result:", result, time);
+  return { result, time };
 }
 
 // Запрос для проверки в `test_kv` (userID → orgID)
@@ -69,7 +85,7 @@ async function checkTestKV(userId, expectedOrgId) {
 
 // Запрос для проверки в `test_json` (orgID → [userID, userID, ...])
 async function checkTestJSON(orgId, userId) {
-  const index = await client.json.arrIndex(`test_json:${orgId}`, '$' , userId);
+  const index = await client.json.arrIndex(`test_json:${orgId}`, "$", userId);
   return index !== -1;
 }
 
@@ -80,27 +96,50 @@ async function testRedis() {
 
   try {
     await client.connect();
-    const testsCount = 10000;
+    const testsCount = process.env.TESTS_COUNT || 1000;
     for (let i = 0; i < testsCount; i++) {
-      const randomUser = savedUsers[Math.floor(Math.random() * savedUsers.length)];
+      const randomUser =
+        savedUsers[Math.floor(Math.random() * savedUsers.length)];
       const { userId, orgId } = randomUser;
 
       // Замер времени для `test_kv`
-      const { time: timeKv } = await measureExecutionTime(checkTestKV, userId, orgId);
-      totalTimeKv += timeKv;
+      if (process.env.TEST_KV === "true") {
+
+        const { time: timeKv } = await measureExecutionTime(
+          checkTestKV,
+          userId,
+          orgId
+        );
+        totalTimeKv += timeKv;
+      }
 
       // Замер времени для `test_json`
-      const { time: timeJson } = await measureExecutionTime(checkTestJSON, orgId, userId);
-      totalTimeJson += timeJson;
+      if (process.env.TEST_JSON === "true") {
+        const { time: timeJson } = await measureExecutionTime(
+          checkTestJSON,
+          orgId,
+          userId
+        );
+        totalTimeJson += timeJson;
+      }
     }
 
     // Рассчитываем среднее время запроса
-    const avgTimeKv = totalTimeKv / 1000;
-    const avgTimeJson = totalTimeJson / 1000;
-    console.log(`-- Finish! --`);
-    console.log(`Tests count: ${testsCount}`);
-    console.log(`Average time for test_kv (Plain Key-Value): ${avgTimeKv.toFixed(5)} ms`);
-    console.log(`Average time for test_json (ARRINDEX in JSON array): ${avgTimeJson.toFixed(5)} ms`);
+    if (process.env.TEST_KV === "true") {
+      const avgTimeKv = totalTimeKv / 1000;
+      console.log(
+        `Average time for test_kv (Plain Key-Value): ${avgTimeKv.toFixed(5)} ms`
+      );
+    }
+
+    if (process.env.TEST_JSON === "true") {
+      const avgTimeJson = totalTimeJson / 1000;
+      console.log(
+        `Average time for test_json (ARRINDEX in JSON array): ${avgTimeJson.toFixed(5)} ms`
+      );
+    }
+
+    console.log("-- Finish! --");
   } catch (err) {
     console.error("Error during Redis testing:", err);
   } finally {
